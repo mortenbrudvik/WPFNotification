07652a4d78801427d9ea477e2b1526bff82cd89b
Morten Brudvik <morten@brudvik.com>
2021-06-13 18:16:04 +0200

    Added support for sending toast message

---
+++ WPFNotification.UI/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace WPFNotification.UI
+{
+    public class MainViewModel
+    {
+        public DelegateCommand ShowNotificationCommand => new DelegateCommand(x => ShowNotification());
+
+        // https://docs.microsoft.com/en-us/windows/uwp/design/shell/tiles-and-notifications/send-local-toast?tabs=uwp
+        private static void ShowNotification()
+        {
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("WPFNotification App")
+                .AddText("Hello!")
+                .Show();
+        }
+    }
+}
